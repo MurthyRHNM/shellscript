34f55215a26844f98eef1519af3b7203901e3c40
MurthyRH <murthyrh94@gmail.com>
2024-04-21 22:10:30 +0530

    modified

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -2,9 +2,11 @@
 
 NUMBER1=$1
 NUMBER2=$2
-
+NO1=$NUMBER1
+NO2=$NUMBER2
 SUM=$(($NUMBER1+$NUMBER2))
+SUB=$(($NO1+$NO2))
 
 echo "first number is : $NUMBER1"
 echo "second number is : $NUMBER2"
-echo "sum of number is : $SUM"
+echo "sum of number is : $SUM"s
