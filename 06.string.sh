7e1b2666d08493b3983347d1177d6ade4db731f0
MurthyRH <murthyrh94@gmail.com>
2024-04-21 18:33:41 +0530

    modified

---
+++ 06.string.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+string_var="Hi team, My name is Murthy, working in TCS, Whitefield Bangalore"
+
+echo "The string value is : ${string_var}"
+echo "The string length is : ${#string_var}"
