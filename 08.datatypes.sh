523ad8eba32e6fe946a4b377f37ccd7fbd08a9cb
MurthyRH <murthyrh94@gmail.com>
2024-04-21 22:15:10 +0530

    modified

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -5,9 +5,13 @@ NUMBER2=$2
 NO1=$NUMBER1
 NO2=$NUMBER2
 SUM=$(($NUMBER1+$NUMBER2))
-SUB=$(($NO1+$NO2))
+SUB=$(($NO1-$NO2))
+MUL=$(($NO1*$NO2))
+DIV=$(($NO1/$NO2))
 
 echo "first number is : $NUMBER1"
 echo "second number is : $NUMBER2"
 echo "sum of number is : $SUM"
 echo "sub of numbers is :$SUB"
+echo "multiplies of number is :$MUL"
+echo "divisoin of number is :$DIV"S
