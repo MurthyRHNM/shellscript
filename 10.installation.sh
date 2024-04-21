92f06c0b34a5f54affd97f693258aa7adadab18d
MurthyRH <murthyrh94@gmail.com>
2024-04-21 23:01:10 +0530

    modified

---
+++ 10.installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [$USER -ne 0]
+ then 
+      echo "Pleae run this script with super user"
+ exit 1
+ else
+    echo "your a super user"
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+
+then 
+    echo "installation is failure"
+    exit 1
+else
+    echo "installation is success"
+fi
+     
