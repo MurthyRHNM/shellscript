90312e58d6fde5e155b4355f0f44953d2c437ba7
MurthyRH <murthyrh94@gmail.com>
2024-04-21 22:07:48 +0530

    modified

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -5,6 +5,6 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo " first number is : $NUMBER1"
-echo " second number is : $NUMBER2"
+echo "first number is : $NUMBER1"
+echo "second number is : $NUMBER2"
 echo "sum of number is : $SUM"
