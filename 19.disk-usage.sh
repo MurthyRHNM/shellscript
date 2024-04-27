f93f93163371c9dd7eeb94a025c60fe8c6fd45e1
MurthyRH <murthyrh94@gmail.com>
2024-04-27 13:07:29 +0530

    modified

---
+++ 19.disk-usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -h | grep xfs)
+DISK_THRESOLD=6
+MESSAGE=" "
+
+#+ it will append the output in MESSAGE variable 
+while IFS = read -l line
+do 
+  USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+  FOLDER=$( echo $line | awk -F " " '{print $NF}')
+  if [ $USAGE -ge $DISK_THRESOLD ]
+  then
+     MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"  
+  fi
+done <<<$DISK_USAGE
+
+echo -e "Message : $MESSAGE"
