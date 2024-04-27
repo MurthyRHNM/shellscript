0f01f1cb80e97f0bcd754ee4166809269876a1c2
MurthyRH <murthyrh94@gmail.com>
2024-04-27 13:40:08 +0530

    modified

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -10,7 +10,8 @@ do
   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
   FOLDER=$(echo $line | awk -F " " '{print $NF}')
   #MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
-  if [ $USAGE -ge $DISK_THRESOLD ]
+  if [ $USAGE -ge $DISK_THRESHOLD ]
+  #if [ $USAGE -ge $DISK_THRESHOLD ]
    then
        MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"  
   fi
