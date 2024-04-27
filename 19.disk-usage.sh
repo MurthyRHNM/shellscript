b8af50644caba169de21009617849fc7da8964e7
MurthyRH <murthyrh94@gmail.com>
2024-04-27 13:27:13 +0530

    modified

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -9,6 +9,7 @@ while IFS= read -r line
 do 
   USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
   FOLDER=$( echo $line | awk -F " " '{print $NF}')
+  MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
   #if [ $USAGE -ge $DISK_THRESOLD ]
    #then
        #MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"  
