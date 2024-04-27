07ad90c0e054e404e1f76c2fa58b10748c982328
MurthyRH <murthyrh94@gmail.com>
2024-04-27 13:29:37 +0530

    modified

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -7,13 +7,13 @@ MESSAGE=""
 #+ it will append the output in MESSAGE variable 
 while IFS= read -r line
 do 
-  USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-  FOLDER=$( echo $line | awk -F " " '{print $NF}')
-  MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
-  #if [ $USAGE -ge $DISK_THRESOLD ]
-   #then
-       #MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"  
- # fi
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+  FOLDER=$(echo $line | awk -F " " '{print $NF}')
+  #MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+  if [ $USAGE -ge $DISK_THRESOLD ]
+   then
+       MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"  
+  fi
 done <<< $DISK_USAGE
 
 echo -e "Message : $MESSAGE"
